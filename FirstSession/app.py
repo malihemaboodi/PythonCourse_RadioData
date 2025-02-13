c994a9c5e0177285986d738e38e3d7a1c68cd9d5
malihemaboodi <m.maboody@gmali.com>
2025-02-13 13:16:54 +0100

    First session

--- FirstSession/app.py
+++ FirstSession/app.py
@@ -1,22 +1,28 @@
 import streamlit as st
 
-# Welcome message
-# This will print a welcoming message to the user when the program starts
-st.title("Funny Sport Name Generator")
-st.write("Welcome to the Funny Sport Name Generator!")
+# Set the page title and icon
+st.set_page_config(page_title="Funny Sport Name Generator", page_icon="⚽")
 
-# Ask the user for a sport-related item or activity
-# The user will input the name of a sport-related item (e.g., a ball, running, jumping, etc.)
-# We capture this input in the variable 'sport_gear'
-sport_gear = st.text_input("What is the name of a sport-related item or activity?")
+# Add a stylish header
+st.markdown("""
+    <h1 style="text-align: center; color: #4CAF50;">Funny Sport Name Generator</h1>
+    <p style="text-align: center; color: #555;">Create your own fun sport name with just a few inputs!</p>
+""", unsafe_allow_html=True)
+
+# Add an image or logo if desired
+# st.image("your_image_url.jpg", use_column_width=True)  # Uncomment and replace with your image URL
+
+# Ask the user for a sport-related item or activity with a nicer input box
+sport_gear = st.text_input("What is the name of a sport-related item or activity?", placeholder="e.g., Ball, Running, Swimming")
 
 # Ask the user for a funny thing (animal, object, or adjective)
-# The user will input the name of a funny thing (e.g., a panda, ninja, banana, etc.)
-# We capture this input in the variable 'funny_thing'
-funny_thing = st.text_input("What is the name of a funny animal, object, or adjective?")
+funny_thing = st.text_input("What is the name of a funny animal, object, or adjective?", placeholder="e.g., Panda, Ninja, Banana")
 
-# Combine the inputs and generate a funny sport name
-# This step combines the sport-related item and the funny thing entered by the user
-# We use string concatenation to join these two inputs with a space in between
-if sport_gear and funny_thing:  # Only generate the name if both inputs are provided
-    st.write(f"Your new sport name could be: {sport_gear} {funny_thing}!")
+# Check if the inputs are filled and display the result with a nice font
+if sport_gear and funny_thing:
+    st.markdown(f"""
+        <h2 style="text-align: center; color: #FF6347;">Your new sport name could be:</h2>
+        <h3 style="text-align: center; color: #FFA500; font-size: 30px;">{sport_gear} {funny_thing}!</h3>
+    """, unsafe_allow_html=True)
+else:
+    st.write("Please enter both the sport-related item and the funny thing to generate your name.")
