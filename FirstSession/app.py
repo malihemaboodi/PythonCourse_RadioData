c4ebf26446144195793611e30d41cd3409c95cb2
malihemaboodi <m.maboody@gmali.com>
2025-02-13 13:50:56 +0100

    First session

--- FirstSession/app.py
+++ FirstSession/app.py
@@ -10,7 +10,8 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # Add an image
-st.image("assets/sport_logo.jpg", use_container_width=True)
+st.image("../assets/sport_logo.jpg", use_container_width=True)
+
 
 
 
