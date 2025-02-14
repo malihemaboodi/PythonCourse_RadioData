e790cf06522c78370021fc0a617960b373d93c76
malihemaboodi <m.maboody@gmali.com>
2025-02-14 16:32:04 +0100

    First session

--- Session4/final_project.py
+++ Session4/final_project.py
@@ -5,23 +5,22 @@ print("Welcome to 🦁 Fun Animal Challenge 🐱")
 #List of animals
 animal_name = ["lion","cat","mouse"]
 
-computer_choice = random.choice(animal_name)
-
-user_choice = input(f"Choose your animal: {animal_name}").lower()
+computer_choice = random.randint(0 , 2)
+user_choice = int(input(f"Choose your animal: Type 0 for Lion🦁 , 1 for Cat🐱 or type 2 for Mouse🐭 "))
 
 if computer_choice == user_choice :
-    print(f"It's a Draw! You both chose {computer_choice}")
-elif user_choice == animal_name[0] and computer_choice == animal_name[1] :
+    print(f"It's a Draw! You both chose {animal_name[computer_choice]}")
+elif user_choice == 0  and computer_choice == 1 :
     print(f"You Win! Your {user_choice} 🦁 beats the computer's  {computer_choice} 🐱 ")
-    if user_choice == animal_name[0] and computer_choice == animal_name[2]:
+    if user_choice == 0 and computer_choice == 2:
         print(f"You Win! Your {user_choice} 🦁 beats the computer's  {computer_choice} 🐭 ")
-elif user_choice == animal_name[1] and  computer_choice == animal_name[0] :
+elif user_choice == 1 and  computer_choice == 0 :
     print(f"You Lose! The computer's {computer_choice} 🦁 beats your {user_choice} 🐱 ")
-    if user_choice == animal_name[1] and  computer_choice == animal_name[2] :
+    if user_choice == 1 and  computer_choice == 2 :
         print(f"You Win! Your {user_choice} 🐱 beats the computer's  {computer_choice} 🐭 ")
-elif user_choice == animal_name[2] and  computer_choice == animal_name[0] :
+elif user_choice == 2 and  computer_choice == 0 :
     print(f"You Lose! The computer's {computer_choice} 🦁 beats your {user_choice} 🐭 ")
-    if user_choice == animal_name[2] and  computer_choice == animal_name[1] :
+    if user_choice == 2 and  computer_choice == 1 :
         print(f"You Lose! The computer's {computer_choice} 🐱 beats your {user_choice} 🐭 ")
 
 
